d47a690a895f7606f06d7d35a9d4e603652a28d9
unknown <andrei.bolboaca@stud.ubbcluj.ro>
2021-10-20 12:02:15 +0300

    primele 3

--- main.py
+++ main.py
@@ -1,106 +1,91 @@
-from cmath import sqrt
 import math
-
-def is_interger(lst):
-    lst_int = []
-    for nr in lst:
-        if nr == int(nr):
-            lst_int.append(int(nr))
-    return lst_int
-
-
-def highest_divk(lst, k):
-    maxd = None
-    for nr in lst:
-        if nr % k == 0:
-            if maxd is None or nr > maxd:
-                maxd = nr
-    return maxd
-
-
-def extract_fractionary(n):
-    return str(n).split('.')[1]
-
-
-def is_palindrome(n):
-    p = 0
-    cn = n
-    while n:
-        p = p*10 + n%10
-        n=n//10
-    return cn==p
+from math import sqrt,trunc
+def nr_pare(lst):
+    """
+    functia determina numarul de elemente pare dintr o lista
+    input:lst:list[int]
+    return: nr(numarul elementelor pare)
+    """
+    nr=0
+    for element in lst:
+        if element % 2 == 0:
+            nr += 1
+    return nr
+def multimi_pare(lst1, lst2):
+    """
+    verifica daca cele doua liste au acelasi numar de elemente pare
+    :param lst1:
+    :param lst2:
+    :return: True/False
+    """
+    nr_pare1 = nr_pare(lst1)
+    nr_pare2 = nr_pare(lst2)
+    if nr_pare1 == nr_pare2:
+        return True
+    return False
 
 
-def palindrome(lst):
-    lst_p=[]
-    for nr in lst:
-        frac=extract_fractionary(nr)
-        if is_palindrome(int(frac)):
-            lst_p.append(nr)
-    return lst_p
+def test_nr_pare():
+    assert multimi_pare([1, 2, 3, 4], [2, 3, 4, 1]) == 1
+    assert multimi_pare([1, 2, 3, 4], [2, 3, 4, 2]) == 0
+    assert multimi_pare([1, 2, 2, 4], [2, 2, 4, 1]) == 1
 
 
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(math.sqrt(n))+1):
-        if n % i == 0:
-            return False
-    return True
+def intersectia(lst1,lst2):
+    """
+    verifica daca un element din prima lista se regaseste in cealalta iar daca da il adauga in lista de intersectie
+    :param lst1:
+    :param lst2:
+    :return:lst3 (lista intersectiei)
+    """
+    lst3=[]
+    for element in lst1:
+         if element in lst2 and element not in lst3:
+             lst3.append(element)
+    return lst3
 
 
-def invert_floats_with_sqrt_of_int_prim(lista):
-    result = []
-
-    for numar in lista:
-        sqrt_nr = sqrt(numar)
-        sqrt_int = math.trunc(sqrt_nr)
-        if is_prime(sqrt_int):
-            numar_str = invert_float_as_string(numar)
-            result.append(numar_str)
-        else:
-            result.append(numar)
-
-    return result
-
-
-def invert_float_as_string(numar):
-    numar_str = str(numar)
-    return numar_str[::-1]
-
+def test_intersectie():
+    assert intersectia([1, 2, 3, 4], [2, 3, 4, 1])==[1, 2, 3, 4]
+    assert intersectia([1, 2, 3, 4], [2, 2, 4, 1]) == [1, 2, 4]
+    assert intersectia([1, 2, 3, 4], [5, 6, 7, 8]) == []
 
 def main():
-    lst = []
+    lst1 = []
+    lst2=[]
     while True:
-        print("1. Citire lista float")
-        print("2. Afisarea tuturor numerelor intregi")
-        print("3. Afisarea celui mai mare numar dvizibil cu un numar dat")
+        print("1. Citire a celor doua multimi intregi")
+        print("2. Afisarea daca cele doua multimi au acelasi numar de elemente pare")
+        print("3. Afisarea intersectiei celor doua multimi")
         print("4. Afisarea numerelor cu partea fractionara palindrom")
         print("5. Afisarea listei obtinute prin inversarea ca string a floaturilor cu "
               "partea intreaga a radicalului nr prim ")
         print("x. Pentru a iesi din program")
         optiune = input("Alegeti optiunea dorita:")
         if optiune == '1':
-            lst_string = input("dati numerele separate prin spatiu :")
+            lst_string = input("dati numerele separate prin spatiu a primei multimi:")
             lst_sep = lst_string.split(' ')
             for nr in lst_sep:
-                lst.append(float(nr))
+                lst1.append(int(nr))
+            lst_string = input("dati numerele separate prin spatiu a celei de a doua multimi:")
+            lst_sep = lst_string.split(' ')
+            for nr in lst_sep:
+                lst2.append(int(nr))
         elif optiune == '2':
-            lst_i = is_interger(lst)
-            print(lst_i)
+            if multimi_pare(lst1,lst2):
+                print("Au acelasi numar de elemente pare")
+            else:
+                print("Nu au acelasi numar de elemente pare")
+
         elif optiune == 'x':
             break
         elif optiune == '3':
-            k = int(input("dati k:"))
-            max_k = highest_divk(lst, k)
-            if max_k is None:
-                print("Nu exista niciun nr divizibil cu k")
+            lst3=intersectia(lst1,lst2)
+            if len(lst3):
+                print(lst3)
             else:
-                print(max_k)
-        elif optiune == '4':
-            lst_pal = palindrome(lst)
-            print(lst_pal)
-        elif optiune == '5':
-            invert_floats_with_sqrt_of_int_prim(lst)
+                print("nu exista elemente in intersectie")
 
+test_nr_pare()
+test_intersectie()
 main()
